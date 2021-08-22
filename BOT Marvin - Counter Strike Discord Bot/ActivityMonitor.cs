276aa49a668f6d78b560e0825893be3e71d55e08
TINHOFER Jakob <jak.tinhofer@billrothgymnasium.at>
2021-08-22 16:36:18 +0200

    added activity monitoring

---
+++ BOT Marvin - Counter Strike Discord Bot/ActivityMonitor.cs
@@ -0,0 +1,36 @@
+﻿using Discord;
+using Discord.Rest;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BOT_Marvin___Counter_Strike_Discord_Bot
+{
+    public class ActivityMonitor
+    {
+        private DiscordRestClient restClient;
+        private DiscordSocketClient client;
+        private SocketGuild guild;
+        public ActivityMonitor(string token, ulong guildID, DiscordSocketClient client)
+        {
+            this.client = client;
+            restClient = new DiscordRestClient();
+            restClient.LoginAsync(TokenType.Bot, token).Wait();
+            guild = client.GetGuild(guildID);
+        }
+
+        public List<ulong> GetActiveUsers()
+        {
+            List<ulong> userIDs = new List<ulong>();
+            foreach(var channel in guild.Channels)
+            {
+                foreach (var user in channel.Users)
+                {
+                    userIDs.Add(user.Id);
+                }
+            }
+            return userIDs;
+        }
+    }
+}
